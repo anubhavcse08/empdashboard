b890728cbc5735a1e41a77e4d63c74354c0ec9c5
Anubhav kumar <akumar102@altimetrik.com>
2018-10-29 14:02:00 +0530

    Updates

---
+++ precache-manifest.0afe22dd2dd1c208f18e04f58d1488e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "464da1b1b7f1cfff92f6278828ad7539",
+    "url": "/anubhavcse08/dashboard/static/media/managericon1.464da1b1.png"
+  },
+  {
+    "revision": "ba0e09f7cfa787a73362c37b03e97e38",
+    "url": "/anubhavcse08/dashboard/static/media/bg2.ba0e09f7.jpg"
+  },
+  {
+    "revision": "0e502cbe709d539d428c",
+    "url": "/anubhavcse08/dashboard/static/js/runtime~main.0e502cbe.js"
+  },
+  {
+    "revision": "3fb6655d96b7b64df706",
+    "url": "/anubhavcse08/dashboard/static/js/main.3fb6655d.chunk.js"
+  },
+  {
+    "revision": "6c25db91ba1e0863ff26",
+    "url": "/anubhavcse08/dashboard/static/js/1.6c25db91.chunk.js"
+  },
+  {
+    "revision": "3fb6655d96b7b64df706",
+    "url": "/anubhavcse08/dashboard/static/css/main.0dd5e263.chunk.css"
+  },
+  {
+    "revision": "a5f5fd77869eef41cdffcaa9b3c3cba9",
+    "url": "/anubhavcse08/dashboard/index.html"
+  }
+];
